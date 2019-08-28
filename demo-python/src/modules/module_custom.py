a199c34c4ab9e09a786bad23f8c6ca1d13c7ee1b
bobit <iloveapi@163.com>
2019-08-28 16:46:21 +0800

    update

--- demo-python/src/modules/module_custom.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# Filename: module_custom.py
-
-def print_func( par ):
-    print ("Hello", par)
-    return
--- demo-python/src/modules/module_matplotlib.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.arange(20)
-y = x**2
-
-plt.plot(x, y)
-plt.show()
